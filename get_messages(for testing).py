1367b1b99f595224578fad09e867624880403f79
Anton <sizov210@gmail.com>
2021-05-13 16:13:00 +0300

    paralrl bot

--- get_messages(for testing).py
+++ get_messages(for testing).py
@@ -2,10 +2,11 @@ from pyrogram import Client, filters
 from time import sleep
 
 
-app = Client('Test')
+app = Client('prod_test')
 app.start()
+app.send_message('AV100_bot', '/help')
+sleep(3)
 msg = app.get_history("AV100_bot", limit=1)
 
 for m in msg:
-    print(m.photo)
-    'https://av100.ru/pay?chatid=458850721&tariff=planform3501&userid=1457195'
+    print(m)
